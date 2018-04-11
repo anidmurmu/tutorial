9071b64cc26039cc6370302c55327f628d723fd8
Anid Murmu <anidmurmu@gmail.com>
2018-04-11 23:40:44 +0530

    15th lecture

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
 
 
 urlpatterns = [
 	path( '', views.home ),
 	path( 'login/', login, {'template_name': 'accounts/login.html'} ),
+	path( 'logout/', logout, {'template_name': 'accounts/logout.html'} ),
+    path('register/', views.register, name='register'),
 ]
